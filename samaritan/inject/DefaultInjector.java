eef7e0558ff2a8468ff5e8b50096dd600dd7130c
Thomas Nappo <technology@live.com>
2014-09-14 19:19:07 -0400

    DefaultInjector is now recursive and can retrieve primitives

--- samaritan/inject/DefaultInjector.java
+++ samaritan/inject/DefaultInjector.java
@@ -1,5 +1,8 @@
 package samaritan.inject;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public final class DefaultInjector extends AbstractInjector {
 
 	public DefaultInjector(Binder binder, Module... modules) {
@@ -7,13 +10,35 @@ public final class DefaultInjector extends AbstractInjector {
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public <T> T getInstance(Class<? extends T> type) {
 		try {
-			return ((Class<? extends T>) getBinder().get(type)).newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
+			Class<? extends T> implementation = getBinder().get(type);
+			Constructor<T> constructor = findConstructor(implementation);
+			Class<?>[] parameterTypes = constructor.getParameterTypes();
+			Object[] parameters = new Object[parameterTypes.length];
+			for (int i = 0; i < parameters.length; i++) {
+				Class<?> parameter = parameterTypes[i];
+				if (parameter.isPrimitive()) {
+					parameters[i] = getBinder().get(parameter);
+					continue;
+				}
+				parameters[i] = getInstance(parameter);
+			}
+			return constructor.newInstance(parameters);
+		} catch (InstantiationException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
 			throw new RuntimeException(e); // delegate
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> Constructor<T> findConstructor(Class<? extends T> implementation)
+			throws NoSuchMethodException, SecurityException {
+		for (Constructor<?> constructor : implementation.getConstructors())
+			if (constructor.isAnnotationPresent(Inject.class))
+				return (Constructor<T>) constructor;
+		return (Constructor<T>) implementation.getConstructor();
 	}
 
 }
