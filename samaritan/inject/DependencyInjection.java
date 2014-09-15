1d066cee1424bd6234b75810b6d8fdfb100dcff9
Thomas Nappo <technology@live.com>
2014-09-14 22:08:40 -0400

    DependencyInjection internal simplification

--- samaritan/inject/DependencyInjection.java
+++ samaritan/inject/DependencyInjection.java
@@ -1,25 +1,24 @@
 package samaritan.inject;
 
+import java.util.stream.Stream;
+
 import samaritan.Immutable;
 import samaritan.Objects;
 
 @Immutable
 public final class DependencyInjection {
-	
+
 	private DependencyInjection() {
 		Objects.notConstructable();
 	}
 
-	private static final Module DUMMY_MODULE = binder -> {};
-
 	public static Injector createInjector() {
-		return createInjector(DUMMY_MODULE);
+		return createInjector(binder -> {});
 	}
 
 	public static Injector createInjector(Module... modules) {
 		Binder binder = new DefaultBinder();
-		for (Module module : modules)
-			binder.install(module);
+		Stream.of(modules).forEach(module -> binder.install(module));
 		return new DefaultInjector(binder, modules);
 	}
 
